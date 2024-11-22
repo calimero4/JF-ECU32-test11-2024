fb725df94ee52f2ffd29b1df61066bfe202db508
calimero4 <m-kritzler2@gmx.de>
2024-11-22 13:19:13 +0100

    Add files via upload
    German + English

---
+++ SRC/main/Langues/de_DE.h
@@ -0,0 +1,103 @@
+/*  de_DE.h
+
+  Copyright (C) 2022  Joachim Franken
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __DE_DE_H_
+#define __DE_De_H_
+
+#define CODE_LANGUE 1031
+#define LANGUE "Deutsch"
+
+  #define B_PARAMECU "Parameter ECU"
+  #define B_PARAM_MOTEUR "Parameter Turbine"
+  #define B_INFORMATION "Informationen"
+  #define B_LOG "Logs"
+  #define B_WIFI "Einstellungen Wifi"
+  #define B_SLIDER "Sliders"
+  #define B_JAUGES "Anzeigen"
+  #define B_CHART "Kurven"
+  #define B_START_ENGINE "Starten Turbine"
+  #define B_STOP_ENGINE "Stoppen Turbine"
+  #define B_MAJ "Aktualisieren"
+  #define B_CUT_WIFI "WIFI Ausschalten"
+
+  #define B_SAVE "Speichern"
+  #define B_RETOUR "Zurueck"
+
+  #define B_CALIBRATION "Kalibrierung"
+  #define B_STARTCALIBRATION "Start Kalibrierung"
+  #define B_STOPCALIBRATION "Stop Kalibrierung"
+
+  #define RD_THROTTLE_TYP "Ventil Gas"
+  #define RD_GLOW_TYP "Kerzentyp"
+  #define RD_START_TYP "Starten"
+  #define RD_PUMP1_TYP "Pumpe 1"
+  #define RD_STARTER_TYP "Startermotor"
+  #define RD_TELEM_TYP "Telemetrie"
+  #define RD_PUMP2_TYP "Pumpe 2"
+  #define CH_AUX_EN "AUX aktiviert"
+  #define CH_LEDS_EN "Leds aktiv"
+
+  #define CT_THROTTLE_STD "Standard"
+  #define CT_THROTTLE_SBUS "SBUS"
+  #define CT_GLOW_GAZ "Gas"
+  #define CT_GLOW_KERO "Kerostart"
+  #define CT_START_MAN "Manuel"
+  #define CT_START_AUTO "Auto"
+  #define CT_PUMP1_DC "DC Motor"
+  #define CT_PUMP1_PPM "PPM Ausgang"
+  #define CT_STARTER_DC "DC Motor"
+  #define CT_STARTER_PPM "PPM Ausgang"
+  #define CT_TELEM_DIS "Deaktiviert"
+  #define CT_TELEM_FR "FrSky"
+  #define CT_TELEM_GRA "Graupner"
+  #define CT_TELEM_FUT "Futaba"
+  #define CT_PUMP2_DC "DC Motor"
+  #define CT_PUMP2_PPM "PPM Ausgang"
+  #define CT_PUMP2_DIS "Deaktiviert"
+
+  #define IN_NAME "Name Trubine"
+  #define IN_GLOWPOWER "Glowpower 0-255"
+  #define IN_RPMMAX "Maximaldrehzahl"
+  #define IN_RPMIDLE "IDLE Drehzahl"
+  #define IN_RPMMIN "Minimaldrehzahl"
+  #define IN_TEMPSTART "Starttemperatur °C"
+  #define IN_TEMPMAX "Maximaltemperatur °C"
+  #define IN_DELAYACC "Acceleration delay (0-30)"
+  #define IN_DELAYDEC "Deceleration delay (0-30)"
+  #define IN_DELAYSTAB "Stability delay (0-30)"
+  #define IN_PUMP1MAX "PWM Max Pumpe 1 (0-1024)"
+  #define IN_PUMP1MIN "PWM Min Pumpe 1 (0-1024)"
+  #define IN_PUMP2MAX "PWM Max Pumpe 2 (0-1024)"
+  #define IN_PUMP2MIN "PWM Min Pumpe 2 (0-1024)"
+  #define IN_VANNE1MAX "PWM Max Ventil 1 (0-255)"
+  #define IN_VANNE2MAX "PWM Max Ventil 2 (0-255)"
+  #define IN_RPMSTARTER "RPM Zuendung (0-10000RPM)"
+  #define IN_MAXSTARTERRPM "RPM Max Strartermotor"
+
+  #define SURE "Sind Sie sicher"
+  #define B_CONFIRM_SAVE "Speicherbestaetigung"
+
+  #define ST_STARTER "Starter"
+  #define ST_PUMP1 "Pumpe 1"
+  #define ST_PUMP2 "Pumpe 2"
+
+
+
+  #define D_DECIMAL_SEPARATOR ","
+
+#endif
---
+++ SRC/main/Langues/en_GB.h
@@ -0,0 +1,103 @@
+/*  en_GB.h
+
+  Copyright (C) 2022  Joachim Franken
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __EN_GB_H_
+#define __EN_GB_H_
+
+#define CODE_LANGUE 2057
+#define LANGUE "English"
+
+  #define B_PARAMECU "Parameter ECU"
+  #define B_PARAM_MOTEUR "Parameter Turbine"
+  #define B_INFORMATION "Informations"
+  #define B_LOG "Logs"
+  #define B_WIFI "Settings Wifi"
+  #define B_SLIDER "Sliders"
+  #define B_JAUGES "Gauges"
+  #define B_CHART "Curves"
+  #define B_START_ENGINE "Start Turbine"
+  #define B_STOP_ENGINE "Stop Turbine"
+  #define B_MAJ "Update"
+  #define B_CUT_WIFI "WIFI off"
+
+  #define B_SAVE "Save"
+  #define B_RETOUR "Back"
+
+  #define B_CALIBRATION "Calibration"
+  #define B_STARTCALIBRATION "Start Calibration"
+  #define B_STOPCALIBRATION "Stop Calibration"
+
+  #define RD_THROTTLE_TYP "Valve Gas"
+  #define RD_GLOW_TYP "Glowplug Type"
+  #define RD_START_TYP "Start"
+  #define RD_PUMP1_TYP "Pump 1"
+  #define RD_STARTER_TYP "Starter"
+  #define RD_TELEM_TYP "Telemetrie"
+  #define RD_PUMP2_TYP "Pump 2"
+  #define CH_AUX_EN "AUX aktivated"
+  #define CH_LEDS_EN "Leds aktiv"
+
+  #define CT_THROTTLE_STD "Standard"
+  #define CT_THROTTLE_SBUS "SBUS"
+  #define CT_GLOW_GAZ "Gas"
+  #define CT_GLOW_KERO "Kerostart"
+  #define CT_START_MAN "Manuel"
+  #define CT_START_AUTO "Auto"
+  #define CT_PUMP1_DC "DC Motor"
+  #define CT_PUMP1_PPM "PPM out"
+  #define CT_STARTER_DC "DC Motor"
+  #define CT_STARTER_PPM "PPM out"
+  #define CT_TELEM_DIS "deactivated"
+  #define CT_TELEM_FR "FrSky"
+  #define CT_TELEM_GRA "Graupner"
+  #define CT_TELEM_FUT "Futaba"
+  #define CT_PUMP2_DC "DC Motor"
+  #define CT_PUMP2_PPM "PPM out"
+  #define CT_PUMP2_DIS "deactivated"
+
+  #define IN_NAME "Name Trubine"
+  #define IN_GLOWPOWER "Glowpower 0-255"
+  #define IN_RPMMAX "max RPM"
+  #define IN_RPMIDLE "IDLE RPM"
+  #define IN_RPMMIN "min RPM"
+  #define IN_TEMPSTART "Start temp. °C"
+  #define IN_TEMPMAX "Maxi temp. °C"
+  #define IN_DELAYACC "Acceleration delay (0-30)"
+  #define IN_DELAYDEC "Deceleration delay (0-30)"
+  #define IN_DELAYSTAB "Stability delay (0-30)"
+  #define IN_PUMP1MAX "PWM Max Pump 1 (0-1024)"
+  #define IN_PUMP1MIN "PWM Min Pump 1 (0-1024)"
+  #define IN_PUMP2MAX "PWM Max Pump 2 (0-1024)"
+  #define IN_PUMP2MIN "PWM Min Pump 2 (0-1024)"
+  #define IN_VANNE1MAX "PWM Max Valve 1 (0-255)"
+  #define IN_VANNE2MAX "PWM Max Valve 2 (0-255)"
+  #define IN_RPMSTARTER "RPM Ignition (0-10000RPM)"
+  #define IN_MAXSTARTERRPM "RPM Max Strarter"
+
+  #define SURE "Are you sure"
+  #define B_CONFIRM_SAVE "confirm saving"
+
+  #define ST_STARTER "Starter"
+  #define ST_PUMP1 "Pump 1"
+  #define ST_PUMP2 "Pump 2"
+
+
+
+  #define D_DECIMAL_SEPARATOR ","
+
+#endif
